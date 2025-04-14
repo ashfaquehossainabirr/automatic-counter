e6cec263951b0677877927b58ef243bf399a19e1
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-04-14 18:27:21 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
-const counter = document.querySelector(".counter")
+// const counter = document.querySelector(".counter")
 
-let count = 0
+// let count = 0
 
-function setCount(num) {
-    let result
+// function setCount(num) {
+//     let result
 
-    if(count === num) {
-        result = count
-        clearInterval(countInterval)
-        console.log("Finished Count: " + result)
-        return counter.innerHTML = result
-    } else {
-        result = count++
-        console.log(result)
-        return counter.innerHTML = result
+//     if(count === num) {
+//         result = count
+//         clearInterval(countInterval)
+//         console.log("Finished Count: " + result)
+//         return counter.innerHTML = result
+//     } else {
+//         result = count++
+//         console.log(result)
+//         return counter.innerHTML = result
+//     }
+// }
+
+// let countInterval = setInterval(() => {
+//     setCount(50)
+// }, 18)
+
+
+
+const counter = document.querySelectorAll(".counter")
+const counterArray = Array.from(counter)
+
+counterArray.map((counterItem => {
+    let count = 0
+
+    function increment() {
+        count++
+        counterItem.innerHTML = count
+
+        if(count === Number(counterItem.dataset.number)) {
+            console.log(counterItem.dataset.item + ": Completed!")
+            clearInterval(countInterval)
+        }
     }
-}
 
-let countInterval = setInterval(() => {
-    setCount(50)
-}, 18)
+    const countInterval = setInterval(increment, counterItem.dataset.time)
+}))
