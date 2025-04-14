e0ac5d4d9ea34eab414d25b7de19e742e36890e2
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-04-14 21:06:50 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -1,43 +1,88 @@
-// const counter = document.querySelector(".counter")
+// const counter = document.querySelectorAll(".counter")
+// const counterArray = Array.from(counter)
+// const audio = new Audio('./audio/alert.mp3')
+// const display = document.querySelector(".output-display")
 
-// let count = 0
+// function startCount() {
+//     counterArray.map((counterItem => {
+//         let count = 0
+    
+//         function increment() {
+//             count++
+//             counterItem.innerHTML = count
+    
+//             if(count === Number(counterItem.dataset.number)) {
+//                 console.log(counterItem.dataset.item + ": Completed!")
+//                 display.style.opacity = "1"
+//                 display.style.visibility = "visible"
+//                 display.innerHTML = `${counterItem.dataset.item}: Completed`
+//                 clearInterval(countInterval)
+//                 audio.play()
 
-// function setCount(num) {
-//     let result
-
-//     if(count === num) {
-//         result = count
-//         clearInterval(countInterval)
-//         console.log("Finished Count: " + result)
-//         return counter.innerHTML = result
-//     } else {
-//         result = count++
-//         console.log(result)
-//         return counter.innerHTML = result
-//     }
+//                 setTimeout(() => {
+//                     display.style.opacity = "0"
+//                     display.style.visibility = "invisible"
+//                     display.innerHTML = ""
+//                 }, 4000)
+//             }
+//         }
+    
+//         const countInterval = setInterval(increment, counterItem.dataset.time)
+//     }))
 // }
 
-// let countInterval = setInterval(() => {
-//     setCount(50)
-// }, 18)
+// function reloadFunc() {
+//     window.location.reload()
+// }
 
 
 
 const counter = document.querySelectorAll(".counter")
 const counterArray = Array.from(counter)
+const audio = new Audio('./audio/alert.mp3')
+const display = document.querySelector(".output-display")
 
-counterArray.map((counterItem => {
-    let count = 0
+function startCount() {
+    counterArray.forEach(counterItem => {
+        let count = 0
+        const countInterval = setInterval(increment, counterItem.dataset.time)
 
-    function increment() {
-        count++
-        counterItem.innerHTML = count
+        function increment() {
+            count++
+            counterItem.innerHTML = count
 
-        if(count === Number(counterItem.dataset.number)) {
-            console.log(counterItem.dataset.item + ": Completed!")
-            clearInterval(countInterval)
+            if (count === Number(counterItem.dataset.number)) {
+                console.log(`${counterItem.dataset.item}: Completed!`)
+                display.style.opacity = "1"
+                display.style.visibility = "visible"
+
+                // Append instead of replace
+                const message = document.createElement("div")
+                message.className = "msg"
+
+                message.innerHTML = `${counterItem.dataset.item}: Completed`
+                display.appendChild(message)
+
+                clearInterval(countInterval)
+                audio.play()
+
+                // Optional: Hide the display after all counters finish
+                const allDone = counterArray.every(c =>
+                    Number(c.innerHTML) === Number(c.dataset.number)
+                )
+
+                if (allDone) {
+                    setTimeout(() => {
+                        display.style.opacity = "0"
+                        display.style.visibility = "hidden"
+                        display.innerHTML = ""
+                    }, 4000)
+                }
+            }
         }
-    }
+    })
+}
 
-    const countInterval = setInterval(increment, counterItem.dataset.time)
-}))
+function reloadFunc() {
+    window.location.reload()
+}
